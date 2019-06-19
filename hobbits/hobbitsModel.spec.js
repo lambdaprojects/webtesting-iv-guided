d38c90ba57ed04c75c1addc2515888719043002d
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-19 10:26:18 -0500

    Completed the test for insert and server.js

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -2,7 +2,7 @@ const Hobbits = require("./hobbitsModel.js");
 const db = require("../data/dbConfig.js");
 
 describe("Hobbits Model", () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     await db("hobbits").truncate();
   });
   describe("Insert", () => {
@@ -12,6 +12,17 @@ describe("Hobbits Model", () => {
       const hobbits = await db("hobbits");
 
       expect(hobbits).toHaveLength(1);
+    });
+
+    it("Should insert provided hobbit multiple times", async () => {
+      let hobbit = await Hobbits.insert({ name: "gaffer" });
+      expect(hobbit.name).toBe("gaffer");
+
+      hobbit = await Hobbits.insert({ name: "Sam" });
+      expect(hobbit.name).toBe("Sam");
+
+      const hobbits = await db("hobbits");
+      expect(hobbits).toHaveLength(2);
     });
   });
 });
